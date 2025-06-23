58560fd1b101466c733d36b752721649ce4ca7cd
anism <muhamadanis10@gmail.com>
2025-06-23 09:39:44 +0700

    Add config for env

---
+++ src/utils/config.js
@@ -0,0 +1,29 @@
+const config = {
+  app: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+  },
+  pg: {
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
+  },
+  jwt: {
+    accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+    refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
+    tokenAge: process.env.ACCESS_TOKEN_AGE,
+  },
+  s3: {
+    bucketName: process.env.AWS_BUCKET_NAME,
+  },
+  rabbitMq: {
+    server: process.env.RABBITMQ_SERVER,
+  },
+  redis: {
+    host: process.env.REDIS_SERVER,
+  },
+};
+
+module.exports = config;
